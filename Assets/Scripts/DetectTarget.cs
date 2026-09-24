5e3e1443e61b5100dabfe4a937366ba14071fe11
Marcoxs22 <ardillascomunitarias@gmail.com>
2026-09-23 20:36:26 -0600

    Puros codigos

--- Assets/Scripts/DetectTarget.cs
+++ Assets/Scripts/DetectTarget.cs
@@ -8,12 +8,15 @@ public class DetectTarget : MonoBehaviour
     [SerializeField]
     private float rayHeighOffset = 0.5f;
     private bool isActive;
-    private System.Action<Transform> onTargetDetected;
-    public System.Action<Transform> OnTargetDetected => onTargetDetected;
-    public void SetRange(bool newActive)
+    public event System.Action<Health> OnTargetDetected;
+    public void SetRange(float newRange)
+    {
+        range = newRange;
+    }
+    public void SetActive(bool newActive)
     {
         isActive = newActive;
-    }
+    } 
     private void Update()
     {
         if (!isActive) return;
@@ -21,7 +24,7 @@ public class DetectTarget : MonoBehaviour
         {
             if (hit.collider.CompareTag(targetTag))
             {
-                onTargetDetected?.Invoke(hit.transform);
+                OnTargetDetected?.Invoke(hit.collider.GetComponent<Health>());
             }
         }
     }
---
+++ Assets/Scripts/Peashooter.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Peashooter : Character
+{
+    private void OnEnable()
+    {
+        health.SetMaxHealth(100);
+        health.Initialize();
+    }
+}
